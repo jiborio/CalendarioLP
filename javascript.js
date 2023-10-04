db9011344a97ccfd1e04a64c2b7365f0f39d0af6
Juan Ignacio Borio <jiborio@hotmail.com>
2023-10-03 23:14:11 -0300

    Agregue JS

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const calendario = document.querySelector('calendar');
+const menuList = document.querySelector('.nav__list');
+const links = document.querySelectorAll('.nav__link');
+
+menu.addEventListener('click', function(){
+
+    menuList.classList.toggle('nav__list--show');
+
+});
+
+
+
